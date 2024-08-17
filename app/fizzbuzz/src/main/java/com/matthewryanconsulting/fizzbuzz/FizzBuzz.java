7ce15eec9f2478278e72c9b5accd1392db92dc7e
Matthew Ryan Granger <mattg1827@gmail.com>
2024-08-17 11:37:49 -0700

    Coding with Abu, complete FizzBuzz solution

--- app/fizzbuzz/src/main/java/com/matthewryanconsulting/fizzbuzz/FizzBuzz.java
+++ app/fizzbuzz/src/main/java/com/matthewryanconsulting/fizzbuzz/FizzBuzz.java
@@ -2,4 +2,19 @@ package com.matthewryanconsulting.fizzbuzz;
 
 public class FizzBuzz {
 
+    public String convert(int number) {
+        if (number % 15 == 0) {
+            return "FizzBuzz";
+        }
+        if (number % 3 == 0) {
+            return "Fizz";
+        }
+        if (number % 5 == 0) {
+            return "Buzz";
+        }
+//        if (number == 15) {
+//            return "FizzBuzz";
+//        }
+        return String.valueOf(number);
+    }
 }
--- app/fizzbuzz/src/test/java/com/matthewryanconsulting/fizzbuzz/FizzBuzzTest.java
+++ app/fizzbuzz/src/test/java/com/matthewryanconsulting/fizzbuzz/FizzBuzzTest.java
@@ -3,13 +3,95 @@ package com.matthewryanconsulting.fizzbuzz;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
 
     private FizzBuzz unit;
 
     @BeforeEach
-    public void setUp() {
+    public void setup() {
         //Arrange
         unit = new FizzBuzz();
+    }
+
+    @Test
+    public void returnOneTest() {
+        //Act
+        int one = 1;
+        String result = unit.convert(one);
+
+        //Assert
+        assertEquals("1", result);
+    }
+
+    @Test
+    public void returnFourteenTest() {
+        //Act
+        int fourteen = 14;
+        String result = unit.convert(fourteen);
+
+        //Assert
+        assertEquals("14", result);
+    }
+
+    @Test
+    public void returnFizzWhenPassedThreeTest() {
+        //Act
+        int three = 3;
+        String result = unit.convert(three);
+
+        //Assert
+        assertEquals("Fizz", result);
+    }
+
+    @Test
+    public void returnFizzWhenMultipleOfThreeTest() {
+        //Act
+        int nine = 9;
+        String result = unit.convert(nine);
+
+        //Assert
+        assertEquals("Fizz", result);
+    }
+
+    @Test
+    public void returnBuzzWhenPassedFiveTest() {
+        //Act
+        int five = 5;
+        String result = unit.convert(five);
+
+        //Assert
+        assertEquals("Buzz", result);
+    }
+
+    @Test
+    public void returnBuzzWhenMultiplesFiveTest() {
+        //Act
+        int ten = 10;
+        String result = unit.convert(ten);
+
+        //Assert
+        assertEquals("Buzz", result);
+    }
+
+    @Test
+    public void returnFizzBuzzWhenPassedFifteenTest() {
+        //Act
+        int fifteen = 15;
+        String result = unit.convert(fifteen);
+
+        //Assert
+        assertEquals("FizzBuzz", result);
+    }
+
+    @Test
+    public void returnFizzBuzzWhenMultiplesFifteenTest() {
+        //Act
+        int thirty = 30;
+        String result = unit.convert(thirty);
+
+        //Assert
+        assertEquals("FizzBuzz", result);
     }
 }
