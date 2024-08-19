6ca0a49cfa255ebe8c8385dd60d7c973ccd741a8
Matthew Ryan Granger <mattg1827@gmail.com>
2024-08-19 14:15:37 -0700

    FB day 2 complete

--- app/fizzbuzz/src/main/java/dev/matthewryan/fizzbuzz/day2/FizzBuzz.java
+++ app/fizzbuzz/src/main/java/dev/matthewryan/fizzbuzz/day2/FizzBuzz.java
@@ -3,15 +3,13 @@ package dev.matthewryan.fizzbuzz.day2;
 public class FizzBuzz {
 
     public String convert(Integer value) {
-        if (value % 15 == 0) {
-            return "FizzBuzz";
-        }
         if (value % 3 == 0) {
+            if (value % 5 == 0)
+                return "FizzBuzz";
             return "Fizz";
         }
-        if (value % 5 == 0) {
+        if (value % 5 == 0)
             return "Buzz";
-        }
         return String.valueOf(value);
     }
 }
