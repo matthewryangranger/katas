b93c4d847e312a9adf890c229507c1289b460ef4
Matthew Ryan Granger <mattg1827@gmail.com>
2024-08-21 15:43:58 -0700

    FBday4 test case for multiples of 3 works

--- app/fizzbuzz/src/main/java/dev/matthewryan/fizzbuzz/day4/FizzBuzz.java
+++ app/fizzbuzz/src/main/java/dev/matthewryan/fizzbuzz/day4/FizzBuzz.java
@@ -2,6 +2,9 @@ package dev.matthewryan.fizzbuzz.day4;
 
 public class FizzBuzz {
     public String convert(int value) {
+        if (value % 3 == 0) {
+            return "Fizz";
+        }
         return String.valueOf(value);
     }
 }
--- app/fizzbuzz/src/test/java/dev/matthewryan/fizzbuzz/day4/FizzBuzzTest.java
+++ app/fizzbuzz/src/test/java/dev/matthewryan/fizzbuzz/day4/FizzBuzzTest.java
@@ -15,4 +15,34 @@ public class FizzBuzzTest {
         // Assert
         assertEquals("1", unit.convert(one));
     }
+
+    @Test
+    public void testConvertIntegerTwotoStringTwo() {
+        // Arrange
+        FizzBuzz unit = new FizzBuzz();
+        // Act
+        Integer one = 1;
+        // Assert
+        assertEquals("1", unit.convert(one));
+    }
+
+    @Test
+    public void testConvertIntegerThreetoStringFizz() {
+        // Arrange
+        FizzBuzz unit = new FizzBuzz();
+        // Act
+        Integer three = 3;
+        // Assert
+        assertEquals("Fizz", unit.convert(three));
+    }
+
+    @Test
+    public void testConvertMultiplesOfIntegerThreetoStringFizz() {
+        // Arrange
+        FizzBuzz unit = new FizzBuzz();
+        // Act
+        Integer nine = 9;
+        // Assert
+        assertEquals("Fizz", unit.convert(nine));
+    }
 }
